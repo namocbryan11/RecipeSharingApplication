5ce4d5c7f534a93ce4f6abcdcad8c591a5fdc233
Bryan Namoc <bryan.namoc123@gmail.com>
2022-05-26 22:33:16 +0800

    new updates

--- RecipeSharingApp/src/main/java/com/example/demo/controller/RecipeController.java
+++ RecipeSharingApp/src/main/java/com/example/demo/controller/RecipeController.java
@@ -3,8 +3,11 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +16,7 @@ import com.example.demo.repository.RecipeRepository;
 
 @RestController
 @RequestMapping("/api/")
+@CrossOrigin("http://localhost:8081/")
 public class RecipeController {
 	
 	@Autowired
@@ -26,14 +30,34 @@ public class RecipeController {
 		System.out.print("Testing2");
 		return recipeRepository.getAllRecipe();
 	}
-	@GetMapping("/recipes/{name}/{id}")
-	public String updateRecipe(@PathVariable String name, @PathVariable int id) {
+	@PostMapping("/recipes/update/{id}")
+	public String updateRecipe(@RequestBody Recipe recipe, @PathVariable int id) {
 		try {
-			recipeRepository.updateRecipe(name,id);
+			recipeRepository.updateRecipe(recipe,id);
 			return "Succesfully Changed!";
 		}catch(Exception e) {
 			return e.getMessage();
 		}
+		 
+	}
+	//creating new recipe
+	@PostMapping("/recipes")
+	public Recipe createRecipe(@RequestBody Recipe recipe) {
+		System.out.print("Recipe: "+ recipe.getDescription());
+		return recipeRepository.createRecipe(recipe);
+	}
+	
+	@PostMapping("/recipes/{id}")
+	public boolean deleteRecipe(@PathVariable int id) {
+		System.out.print("Id to delete: " + id);
+		try {
+			recipeRepository.deleteRecipe(id);
+			return true;
+		}catch(Exception e) {
+			System.out.print(e.getMessage());
+			return false;
+		}
 		
 	}
+	
 }
---
+++ RecipeSharingApp_FE/frontend/src/common/constants/variables.js
@@ -0,0 +1,5 @@
+const RECIPE_URL = "http://localhost:8080/api/recipes";
+const UPDATE_RECIPE_URL = "http://localhost:8080/api/recipes/update";
+
+module.exports.RECIPE_URL = RECIPE_URL;
+module.exports.UPDATE_RECIPE_URL = UPDATE_RECIPE_URL;
